8fb00bcf096cba27a724616a5a63288846266f2b
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-05-16 05:03:53 -0400

    Added Client

--- Makefile
+++ Makefile
@@ -5,7 +5,19 @@ EXEC=server client
 
 all: client server
 
-connect.o: Connect.c Connect.h
-	$(CC) $(CFLAGS) -c Connect.c -o connect.o
+server: server.o connect.o
+	$(CC) server.o connect.o $(PFLAGS) -o server
+
+client: client.o connect.o
+	$(CC) client.o connect.o $(PFLAGS) -o client
+
+server.o: Server.c
+	$(CC) $(CFLAGS) -c Server.c -o server.o
+
+client.o: Client.c
+	$(CC) $(CFLAGS) -c Client.c -o client.o
+
+connect.o: Connect/Connect.c
+	$(CC) $(CFLAGS) -c Connect/Connect.c -o connect.o
 clean:
 	rm -f $(EXEC) *.o *~
--- Server.c
+++ Server.c
@@ -21,7 +21,7 @@ typedef struct client{
 void* handle_client(void*);
 
 int main(int argc, char* argv[]){
-  int server_socket, client_socket, clients[MAX_CLIENTS], client_count = 0, port = PORT;
+  int server_socket, client_socket, client_count = 0, port = PORT;
   int num_clients = -99999;
   size_t num_bytes, bytes_left, bytes_per_partition;
   struct sockaddr_in server;
@@ -38,7 +38,7 @@ int main(int argc, char* argv[]){
     else if(!strcmp(argv[i],"-n") && argc >= i + 1)
       num_clients = atoi(argv[i + 1]);
     else if(!strcmp(argv[i],"-f") && argc >= i + 1)
-      port = atoi(argv[i + 1]);
+      file_name = argv[i + 1];
     else{
       printf("Usage: %s [-n] num_clients [-f] file_name [-i] ip_addr [-p] port\n", argv[0]);
       printf("Default: ip = 127.0.0.0.1, port = 4444\n");
@@ -61,35 +61,32 @@ int main(int argc, char* argv[]){
   bind_connection(server_socket,(struct sockaddr*)&server);
   listen_for_connection(server_socket, MAX_CLIENTS);
 
-
-  /* Accept connections from k clients */
-  while((client_socket = accept(server_socket,(struct sockaddr *)&server,&sckln))
-	&& client_count < num_clients){
-    clients[client_count] = client_socket;
-    printf("[+] Client %d connected! \n", client_count++);
-  }
-  
   assert(stat(file_name, &st) != -1);
-
+  
   num_bytes = bytes_left = st.st_size;
   bytes_per_partition = num_bytes / num_clients;
   
-  for(int i = 0; i < num_clients; ++i){
-    Client c;
-    c.client_socket = clients[i];
-    c.start_pos = num_bytes - bytes_left;
-    c.end_pos = (i == num_clients - 1) ? num_bytes : c.start_pos + bytes_per_partition;
-    c.file_name = file_name;
-    c.client_number = i;
-    bytes_left -= bytes_per_partition;
+  /* Accept connections from k clients */
+  while((client_count < num_clients) &&
+	(client_socket = accept(server_socket,(struct sockaddr *)&server,&sckln))){
 
-    assert(pthread_create(tids + i, NULL, handle_client, &c) < 0);
+    Client c;
+    c.client_socket = client_socket;
+    c.start_pos = num_bytes - bytes_left;
+    c.end_pos = (client_count  == num_clients - 1) ? num_bytes : c.start_pos + bytes_per_partition;
+    c.file_name = file_name;
+    c.client_number = client_count;
+    bytes_left -= bytes_per_partition;
+    
+    assert(pthread_create(tids + client_count, NULL, handle_client, &c) == 0);
+    printf("[+] Client %d connected! \n", client_count++);
   }
+  
   
   for(int i = 0; i < num_clients; ++i)
     pthread_join(tids[i], NULL);
   
-  printf("[-] Transfer complete. Closing Server Socket.\n");
+  fprintf(stdout, "[-] Transfer complete. Closing Server Socket.\n");
   close(server_socket);
   return 0;
 }
@@ -102,16 +99,15 @@ void* handle_client(void* args){
   int sent_bytes, fd, bytes_remaining = bytes_to_send;
   
   memset(buffer, 0, sizeof(buffer));
-  sprintf(buffer, "Sending: %d bytes.", bytes_to_send);
+  sprintf(buffer, "You are client #%d. Sending (%d) bytes.", c->client_number, bytes_to_send);
   assert(send(c->client_socket, buffer, BUF, 0) > 0);
-
   assert((fd = open(c->file_name, O_RDONLY)) != 1);
   
   while((bytes_remaining > 0) &&
 	(sent_bytes = sendfile(c->client_socket, fd, &offset, BUF)) > 0){
+    bytes_remaining -= sent_bytes;
     fprintf(stdout, "[+] %lf%c: ", 100.0 * (1 - (double)bytes_remaining/bytes_to_send), '%');
     fprintf(stdout, "Sent %d bytes to client %d\n", sent_bytes, c->client_number);
-    bytes_remaining -= sent_bytes;
   }
 
   /*TODO Send MD5 checksum of the portion of the file*/
